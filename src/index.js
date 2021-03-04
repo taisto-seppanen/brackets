252d1205cad233b876621ed16149067effeda102
taisto-seppanen <taistopetrseppanen@gmail.com>
2021-03-04 13:14:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+
+    const arr = [];
+    let answer = str;
+
+    for (let x of bracketsConfig) {
+        if (parseInt(str)) {
+            arr.push(`(${x[0]}${x[1]})`);
+        } else {
+            arr.push("(\\" + x[0] + "\\" + x[1] + ")");
+        }
+    }
+
+    const pattern = arr.join("|");
+
+    const re = new RegExp(pattern, "g");
+
+    const lenarr = [-1];
+    lenarr.push(str.length);
+    let i = 1;
+    let curlen = 0;
+
+    while (lenarr[i - 1] !== lenarr[i]) {
+        answer = answer.replace(re, "");
+        curlen = answer.length;
+        lenarr.push(curlen);
+        i++;
+    }
+
+    return !answer;
+};
